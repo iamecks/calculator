007b266583890dbb32dcfaa1b2d7e79383644a63
Xerxis Tan <xerxis.tan@metergysolutions.com>
2023-01-04 00:34:47 -0500

    refactor: functions rename, cover some edge cases

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,155 +1,161 @@
 <template>
-  <div class="container">
-    <form>
-      <div class="row gx-1 mt-5">
-        <div class="form-group">
-          <label for="result">Result</label>
-          <input
-            v-if="operator && secondGivenNumber"
-            type="number"
-            class="form-control"
-            id="result"
-            v-model="secondGivenNumber"
-          />
+  <div class="container col-2 bg-black text-white mt-5 mx-auto px-1 rounded-1">
+    <!-- Result/Input Display -->
+    <div class="row gx-1 mt-1">
+      <h1 class="text-white text-end">{{ state.displayValue }}</h1>
+    </div>
 
-          <input
-            v-else
-            type="number"
-            class="form-control"
-            id="result"
-            v-model="firstGivenNumber"
-          />
-        </div>
+    <!-- Row #1 -->
+    <div class="row gx-1 mt-1">
+      <div class="col" @click="resetCurrentDisplayValue">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">CE</div>
       </div>
+      <div class="col" @click="resetAll">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">AC</div>
+      </div>
+      <div class="col" @click="deleteLatestDigit">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">DEL</div>
+      </div>
+      <div class="col" @click="performOperation('/')">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">÷</div>
+      </div>
+    </div>
 
-      <div class="row gx-1 mt-1">
-        <div class="col" @click="clearAll">
-          <div class="p-3 border bg-light">CE</div>
-        </div>
-        <div class="col" @click="clearAll">
-          <div class="p-3 border bg-light">C</div>
-        </div>
-        <div class="col" @click="clearAll">
-          <div class="p-3 border bg-light">DEL</div>
-        </div>
-        <div class="col" @click="selectOperator('/')">
-          <div class="p-3 border bg-light">/</div>
-        </div>
+    <!-- Row #2 -->
+    <div class="row gx-1 mt-1">
+      <div class="col" @click="updateDisplay('7')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">7</div>
       </div>
-      <div class="row gx-1 mt-1">
-        <div class="col" @click="concatInputVal('7')">
-          <div class="p-3 border bg-light">7</div>
-        </div>
-        <div class="col" @click="concatInputVal('8')">
-          <div class="p-3 border bg-light">8</div>
-        </div>
-        <div class="col" @click="concatInputVal('9')">
-          <div class="p-3 border bg-light">9</div>
-        </div>
-        <div class="col" @click="selectOperator('x')">
-          <div class="p-3 border bg-light">x</div>
-        </div>
+      <div class="col" @click="updateDisplay('8')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">8</div>
       </div>
-      <div class="row gx-1 mt-1">
-        <div class="col" @click="concatInputVal('4')">
-          <div class="p-3 border bg-light">4</div>
-        </div>
-        <div class="col" @click="concatInputVal('5')">
-          <div class="p-3 border bg-light">5</div>
-        </div>
-        <div class="col" @click="concatInputVal('6')">
-          <div class="p-3 border bg-light">6</div>
-        </div>
-        <div class="col" @click="selectOperator('-')">
-          <div class="p-3 border bg-light">-</div>
-        </div>
+      <div class="col" @click="updateDisplay('9')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">9</div>
       </div>
-      <div class="row gx-1 mt-1">
-        <div class="col" @click="concatInputVal('1')">
-          <div class="p-3 border bg-light">1</div>
-        </div>
-        <div class="col" @click="concatInputVal('2')">
-          <div class="p-3 border bg-light">2</div>
-        </div>
-        <div class="col" @click="concatInputVal('3')">
-          <div class="p-3 border bg-light">3</div>
-        </div>
-        <div class="col" @click="selectOperator('+')">
-          <div class="p-3 border bg-light">+</div>
-        </div>
+      <div class="col" @click="performOperation('x')">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">x</div>
       </div>
-      <div class="row gx-1 mt-1">
-        <div class="col" @click="concatInputVal('n')">
-          <div class="p-3 border bg-light">+/-</div>
-        </div>
-        <div class="col" @click="concatInputVal('0')">
-          <div class="p-3 border bg-light">0</div>
-        </div>
-        <div class="col" @click="concatInputVal('.')">
-          <div class="p-3 border bg-light">.</div>
-        </div>
-        <div class="col" @click="checkResult">
-          <div class="p-3 border bg-light">=</div>
-        </div>
+    </div>
+
+    <!-- Row #3 -->
+    <div class="row gx-1 mt-1">
+      <div class="col" @click="updateDisplay('4')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">4</div>
       </div>
-    </form>
+      <div class="col" @click="updateDisplay('5')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">5</div>
+      </div>
+      <div class="col" @click="updateDisplay('6')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">6</div>
+      </div>
+      <div class="col" @click="performOperation('-')">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">-</div>
+      </div>
+    </div>
+
+    <!-- Row #4 -->
+    <div class="row gx-1 mt-1">
+      <div class="col" @click="updateDisplay('1')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">1</div>
+      </div>
+      <div class="col" @click="updateDisplay('2')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">2</div>
+      </div>
+      <div class="col" @click="updateDisplay('3')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">3</div>
+      </div>
+      <div class="col" @click="performOperation('+')">
+        <div class="h6 p-3 mb-0 bg-dark text-center rounded-1">+</div>
+      </div>
+    </div>
+
+    <!-- Row #5 -->
+    <div class="row gx-1 mt-1">
+      <div class="col mb-1" @click="updateDisplay('!')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">+/-</div>
+      </div>
+      <div class="col" @click="updateDisplay('0')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">0</div>
+      </div>
+      <div class="col" @click="updateDisplay('.')">
+        <div class="h6 p-3 mb-0 bg-secondary text-center rounded-1">.</div>
+      </div>
+      <div class="col" @click="performOperation('=')">
+        <div class="h6 p-3 mb-0 bg-primary text-center rounded-1">=</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 
-const firstGivenNumber = ref("");
-const secondGivenNumber = ref("0");
-const operator = ref("");
+const INITIAL_STATE = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-function clearAll() {
-  firstGivenNumber.value = "";
-  secondGivenNumber.value = "0";
-  operator.value = "";
-}
+let state = reactive({ ...INITIAL_STATE });
 
-function concatInputVal(inputVal) {
-  // Clear placeholder "0"
-  if (secondGivenNumber.value === "0") secondGivenNumber.value = "";
-
-  // If operator already concat on latest number
-  if (operator.value)
-    secondGivenNumber.value = secondGivenNumber.value.concat(inputVal);
-  // Default: only on 1st number
-  else firstGivenNumber.value = firstGivenNumber.value.concat(inputVal);
-}
-
-function selectOperator(inputOperator) {
-  if (secondGivenNumber.value) firstGivenNumber.value = secondGivenNumber.value;
-  operator.value = inputOperator;
-}
-
-function checkResult() {
-  const parsed1stNum = parseInt(firstGivenNumber.value);
-  const parsed2ndNum = parseInt(secondGivenNumber.value);
-  const key = operator.value;
-
-  switch (key) {
-    case "/":
-      secondGivenNumber.value = parsed1stNum / parsed2ndNum;
-      break;
-
-    case "x":
-      secondGivenNumber.value = parsed1stNum * parsed2ndNum;
-      break;
-
-    case "-":
-      secondGivenNumber.value = parsed1stNum - parsed2ndNum;
-      break;
-
-    case "+":
-      secondGivenNumber.value = parsed1stNum + parsed2ndNum;
-      break;
-
-    default:
-      alert("Something Wrong");
-      break;
+function updateDisplay(value) {
+  if (state.waitingForSecondOperand) {
+    state.displayValue = value;
+    state.waitingForSecondOperand = false;
+  } else {
+    state.displayValue =
+      state.displayValue === "0" ? value : state.displayValue + value;
   }
+}
+
+function performOperation(newOperator) {
+  if (state.firstOperand == null) {
+    state.firstOperand = parseFloat(state.displayValue);
+    state.waitingForSecondOperand = true;
+    state.operator = newOperator;
+  } else if (newOperator === "=") {
+    const result = eval(
+      `${state.firstOperand} ${state.operator} ${state.displayValue}`
+    );
+    resetAll();
+    state.displayValue = String(result);
+  } else if (state.operator) {
+    // Check previous operator !=
+    if (state.operator === "=") {
+      alert("Invalid Operand!");
+      resetAll();
+      return;
+    }
+
+    const result = eval(
+      `${state.firstOperand} ${state.operator} ${state.displayValue}`
+    );
+    state.displayValue = String(result);
+    state.firstOperand = result;
+    state.waitingForSecondOperand = true;
+    state.operator = newOperator;
+  }
+}
+
+function resetAll() {
+  state.displayValue = INITIAL_STATE.displayValue;
+  state.firstOperand = INITIAL_STATE.firstOperand;
+  state.waitingForSecondOperand = INITIAL_STATE.waitingForSecondOperand;
+  state.operator = INITIAL_STATE.operator;
+}
+
+function resetCurrentDisplayValue() {
+  state.displayValue = "0";
+}
+
+function deleteLatestDigit() {
+  const displayValue = state.displayValue;
+
+  // Trim off last digit
+  state.displayValue = displayValue.substring(0, displayValue.length - 1);
+
+  // If empty set to the default placeholder value
+  if (state.displayValue === "") state.displayValue = "0";
 }
 </script>
